daa8d6dfa4766bffc74dc00057f3396ded6b2a30
legGalaxyCode <alexey.zhavoronkov2001@gmail.com>
2020-02-01 23:52:55 +0300

    first from console

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,37 +1,27 @@
 #include<iostream>
 #include<cmath>
+#include<fstream>
 using namespace std;
+
+
 
 int main() {
 	setlocale(LC_ALL, "Rus");
-	cout << "\"(1 + x)^n >= (1 + xn)\"\n" << "Докажем для некоторого n\n";
+	ifstream file;
+	file.open("C:\\Users\\alexe\\Documents\\My Games\\Terraria\\Worlds\\1.wld");
 
-	int counter;
-	int n;
-	int x;
-	long int a_avg, g_avg;
-
-	cout << "Введите n:\t"; cin >> n;
-	cout << "Введите x:\t"; cin >> x;
-
-	counter = 0;
-
-	int* npos = new int[n];
-	for (int i = 1; i != n; i++) {
-		npos[counter] = i;
-		counter += 1;
-		if (counter == (n-1)) {
-			counter = 0;
-		};
+	if (file.is_open()) {
+		cout << "Kk" << endl;
+	}
+	else {
+		cout << "Bad" << endl;
 	};
 
-	cout << "Для x =\t" << x << " неравенство принимает вид:\n";
-	for (int i = 1; i <= n; i++) {
-		a_avg = pow((1 + x), i);
-		g_avg = (1 + x * i);
-		cout << "При n =\t" << i << "\t" << a_avg << ">=" << g_avg << "\n";
-	};
+	int n = 10;
+	char* buffer = new char[n + 1]; buffer[n] = 0;
+	file.read(buffer, n);
+	cout << buffer;
+	delete[] buffer;
 
-	delete[] npos;
 	return NULL;
 }
